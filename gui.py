c130836a96117bceed930e41fc8f075b0af21774
Tony (Juntao) Hu <807769469hjt@gmail.com>
2021-04-14 16:17:57 -0400

    Finish second version of gui

--- gui.py
+++ gui.py
@@ -1,15 +1,18 @@
 """GUI for autocorrect."""
 import tkinter as tk
-from tkinter.constants import INSERT
 
+from backend import Backend
+from bktree import make_bktree_from_file
+from levenshtein_automaton import LevenshteinBackend
 from trie import Trie, make_trie_from_file
-from bktree import BKTree, make_bktree_from_file
+
 
 class App(tk.Frame):
     """The main GUI class."""
-    _text_area: tk.Text
+    text_area: tk.Text
+    suggestions: tk.Label
+
     _status_bar: tk.PanedWindow
-    _suggestions: tk.Label
     _input_handler: "ACHandler"
 
     def __init__(self, master=None):
@@ -25,8 +28,8 @@ class App(tk.Frame):
 
     def _add_elements(self) -> None:
         """Add the main elements onto the frame."""
-        self._text_area = tk.Text(self, relief=tk.FLAT, wrap=tk.WORD)
-        self._text_area.pack(expand=1, fill=tk.BOTH)
+        self.text_area = tk.Text(self, relief=tk.FLAT, wrap=tk.WORD)
+        self.text_area.pack(expand=1, fill=tk.BOTH)
 
     def _add_menu(self) -> None:
         """Add the menu onto the frame."""
@@ -42,17 +45,18 @@ class App(tk.Frame):
         # setting borderwidth doesn't seem necessary
         self._status_bar = tk.PanedWindow(self, borderwidth=0)
 
-        self._suggestions = tk.Label(self._status_bar, text='Type something...')
-        self._status_bar.add(self._suggestions)
+        self.suggestions = tk.Label(self._status_bar, text='Type something...')
+        self._status_bar.add(self.suggestions)
+        self.suggestions.pack(fill=tk.X, expand=1)
 
         self._status_bar.pack(side=tk.BOTTOM)
 
     def _bind_events(self) -> None:
         """Bind events to the handler."""
-        # TODO: change this
-        self._input_handler = BKTreeHandler(self)
+        self._input_handler = ACHandler(self, 'lv')
         for event, func in self._input_handler.BINDINGS:
-            self._text_area.bind(event, func)
+            self.text_area.bind(event, func)
+
 
 class ACHandler:
     """An abstract class that handles autocorrect/autocomplete functionality
@@ -62,37 +66,52 @@ class ACHandler:
     _app: App
     _ta: tk.Text
 
-    # dictionary of correct words
-    _trie: Trie
+    # CHANGE THIS FOR A DIFFERENT DICTIONARY
+    DICT_FILE = 'big_dictionary.txt'
 
-    def __init__(self, app: App) -> None:
+    _trie: Trie                 # dictionary of correct words
+    _backend: Backend
+    _prev_word: str
+    _suggestions: list[str]     # keeps track of the current suggestions
+
+    def __init__(self, app: App, which: str) -> None:
+        """Note: which in {'lv', 'bk'}"""
         self.BINDINGS = [
-            ('<KeyRelease>', self._key_pressed),
-            ('<ButtonRelease>', self._button_pressed)
+            ('<KeyRelease>', self._key_released),
+            ('<ButtonRelease>', self._button_pressed),
+            ('<Control-Key>', self._replace)
         ]
         self._app = app
-        self._ta = self._app._text_area
+        self._ta = self._app.text_area
         self._ta.mark_set('start', '1.0')
         self._ta.mark_gravity('start', tk.LEFT)
         self._ta.tag_config('cur', foreground='red')
 
-        self._trie = make_trie_from_file('dictionary.txt')
+        self._trie = make_trie_from_file(self.DICT_FILE)
+        if which == 'lv':
+            self._backend = LevenshteinBackend(self._trie)
+        else:
+            self._backend = make_bktree_from_file(self.DICT_FILE)
 
-    def _key_pressed(self, event: tk.Event) -> None:
+        self._prev_word = ''
+        self._suggestions = []
+
+    def _key_released(self, event: tk.Event) -> None:
         """Handle updates associated with key presses, such as updating suggestions."""
-        # TODO: backspace doesn't work yet
-        # TODO: moving cursor doesn't work yet
-        # this is to avoid it accepting values like 'space'
-        # if len(event.keysym) == 1 and event.keysym.isalnum():
-        #     return
-        # otherwise
-        # self._ta.tag_add('cur', self._ta.index('start'), tk.END)
         self._set_word_start()
         self._update_suggestions()
 
     def _button_pressed(self, event: tk.Event) -> None:
         self._set_word_start()
         self._update_suggestions()
+
+    def _replace(self, event: tk.Event) -> None:
+        if event.keysym in '123456':
+            choice = int(event.keysym)
+            # valid choice
+            if choice <= len(self._suggestions):
+                self._ta.delete('start', tk.INSERT)
+                self._ta.insert('start', self._suggestions[choice - 1])
 
     def _set_word_start(self) -> None:
         """Set the mark 'start' to the beginning of the word containing or right before the cursor.
@@ -110,47 +129,40 @@ class ACHandler:
         else:
             self._ta.mark_set('start', cursor_ind + ' wordstart')
 
-
-    # TODO: note on some improvements
-    # Learn the word as soon as the user finishes typing
-    # Prioritize completion over correction
-    # no past tense or something?
     def _update_suggestions(self) -> None:
-        """Update self._app._suggestions to show autocorrect/autocomplete options."""
-        raise NotImplementedError
-
-
-class BKTreeHandler(ACHandler):
-    """A class that implements ACHandler using a BKTree."""
-
-    BINDINGS: list
-    _bktree: BKTree
-
-    def __init__(self, app: App) -> None:
-        super().__init__(app)
-        self._bktree = make_bktree_from_file('dictionary.txt')
-
-    # def key_pressed(self, event: tk.Event) -> None:
-    #     pass
-
-    def _update_suggestions(self) -> None:
-        cur_word = self._ta.get(self._ta.index('start'), tk.INSERT)
+        cur_word = self._ta.get('start', tk.INSERT)
         print('-> ' + cur_word)
-        # print(self._ta.index('start'))
+
+        if cur_word == self._prev_word:
+            return
+        else:
+            self._prev_word = cur_word
 
         # by the specification of 'wordstart', only alphanumeric characters and '_' are
         # connected to form words
         # do not give suggestions on punctuation, etc.
         if not cur_word or (len(cur_word) == 1 and not cur_word.isalnum()):
-            self._app._suggestions['text'] = 'No suggestions...'
-        elif not self._trie.lookup_word(cur_word):
-            sug = self._bktree.get_similar_words_ordered(cur_word)
-            if not sug:
-                self._app._suggestions['text'] = 'No suggestions...'
-            else:
-                self._app._suggestions['text'] = str(sug[0])
-        else:   # if the word is correctly spelled
-            self._app._suggestions['text'] = '...'
+            self._suggestions = []
+        else:
+            completions = self._trie.get_suggestions(cur_word, 3)
+            corrections = []
+            # don't offer corrections if the word is spelled correctly
+            if not self._trie.lookup_word(cur_word):
+                corrections = self._backend.get_suggestions(cur_word, 3)
+            self._suggestions = completions + corrections
+
+        self._app.suggestions['text'] = self._format_suggestions(
+            self._suggestions)
+
+    def _format_suggestions(self, sug: list[str]):
+        """A helper for formatting suggestions."""
+        output = ''
+        for ind, word in enumerate(sug):
+            output += f'{ind + 1}: {word}  '
+
+        if not output:
+            return 'No suggestions...'
+        return output
 
 
 if __name__ == '__main__':
